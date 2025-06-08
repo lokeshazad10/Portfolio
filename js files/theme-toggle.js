b27b56bb2cf55c37308b359193f53ba863994199
lokeshazad10 <lokeshazad10@gmail.com>
2025-06-08 21:14:28 +0530

    Initial commit

--- js files/theme-toggle.js
+++ js files/theme-toggle.js
@@ -1,36 +1,3 @@
-// const toggleButton = document.getElementById('toggleTheme');
-// const image = document.getElementById('theme-png');
-// // const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-// function setTheme() {
-//       if(image.src.includes('./assets/moon.png')){
-//         image.src = './assets/sun.png';
-//       }else{
-//         image.src = './assets/moon.png';
-//       }
-//     }
-
-// // function toggleTheme() {
-// //       const currentTheme = document.body.classList.contains('dark') ? 'dark' : 'light';
-// //       const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-// //       setTheme(newTheme);
-// //     }
-
-// //     // Set initial theme
-// const savedTheme = localStorage.getItem('theme');
-//     if (savedTheme) {
-//       setTheme(savedTheme);
-//     } else {
-//       setTheme(prefersDark ? 'dark' : 'light');
-//     }a
-// // 
-
-// var icon = document.getElementById("toggleTheme");
-
-// icon.addEventListener("click", ()=>{
-//     document.body.classList.toggle("dark-theme");
-// })
-
 const root = document.documentElement;
     const toggleSwitch = document.getElementById('themeSwitch');
 
@@ -39,7 +6,7 @@ const root = document.documentElement;
       const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
       const isDark = savedTheme === 'dark' || (!savedTheme && prefersDark);
 
-      root.classList.toggle('dark', isDark);
+      root.classList.toggle('dark-theme', isDark);
       toggleSwitch.checked = isDark;
     }
 
